2312f458ef96edeecee2af52aa19f27333d4d1ec
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-09 10:15:09 -0400

    changes to index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://upenn-cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareaaddresses?query=Philadelphia&offset=0';
+    var requestUrl = 'https://upenn-cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas?query='+userInput+'&limit=50&offset=0&full=true';
   
     fetch(requestUrl, { headers: { apikey: "20f84e32-f5d0-480b-9f55-dd5f515af6af" } })
       .then(function (response) {
