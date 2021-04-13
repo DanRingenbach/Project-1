288456942c0218da718c535ab7a8e35409285732
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-12 20:03:06 -0400

    adding AQI API and functionality

--- script.js
+++ script.js
@@ -11,16 +11,40 @@ $('#select-city').click(function (event) {
   getApi();
 
   function getApi() {
-    var requestUrl = 'https://upenn-cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas?query=' + userInput + '&limit=50&offset=0&full=true';
-
-    fetch(requestUrl, { headers: { apikey: "20f84e32-f5d0-480b-9f55-dd5f515af6af" } })
+    var recAreaUrl = 'https://upenn-cors-anywhere.herokuapp.com/https://ridb.recreation.gov/api/v1/recareas?query=' + userInput + '&limit=5&offset=0&full=true';
+    fetch(recAreaUrl, { headers: { apikey: "20f84e32-f5d0-480b-9f55-dd5f515af6af" } })
       .then(function (response) {
         return response.json();
       })
-        .then(function (data) {
+      .then(function (data) {
         console.log(data)
         var name = $('<h3>').text(data.RECDATA[0].RecAreaName)
         $('#rec-index').append(name)
       })
+
+    var locationAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + userInput + '&key=31fdf0bb1c3d44a3860a7b19963a2ac9'
+    fetch(locationAPI)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        getAQI(data.results[0].geometry.lat, data.results[0].geometry.lng)
+        // console.log(data.results[0].geometry.lat,data.results[0].geometry.lng)
+      })
+
   }
 })
+
+function getAQI(lat, lon) {
+  var AQIUrl = 'https://upenn-cors-anywhere.herokuapp.com/http://api.airvisual.com/v2/nearest_city?lat=' + lat.toFixed(2) + '&lon=' + lon.toFixed(2) + '&key=8da422fd-653c-45f1-bce2-ca27fc2c8b30'
+  console.log(AQIUrl)
+  fetch(AQIUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      console.log(data.data.current.pollution.aqius)
+    })
+}
