35106d221289a42bc9bd1b2425baa2a55f640c01
Dan Ringenbach <danringenbach.art@gmail.com>
2021-04-16 12:56:55 -0400

    final commit

--- script.js
+++ script.js
@@ -8,10 +8,7 @@ var headOne = document.querySelector('h1');
 var AQI = document.querySelector('#AQI')
 var REC = document.querySelector('#recArea')
 
-
-
 backBtn.addEventListener('click', backButton);
-
 
 $('#input-button').click(function (event) {
   event.preventDefault();
@@ -19,10 +16,8 @@ $('#input-button').click(function (event) {
   $('#AQI').empty();
   
   var userInput = $('#input-search').val()
-  console.log(userInput)
-  
-
   var locationAPI = 'https://api.opencagedata.com/geocode/v1/json?q=' + userInput + '&key=31fdf0bb1c3d44a3860a7b19963a2ac9'
+  
   fetch(locationAPI)
     .then(function (response) {
       return response.json();
@@ -34,7 +29,7 @@ $('#input-button').click(function (event) {
       } else {
         getRecData(data.results[0].geometry.lat, data.results[0].geometry.lng)
       }
-      // console.log(data.results[0].geometry.lat,data.results[0].geometry.lng)
+      
     })
 
 });
@@ -42,14 +37,11 @@ $('#input-button').click(function (event) {
 
 function getAQI(lat, lon) {
   var AQIUrl = 'https://upenn-cors-anywhere.herokuapp.com/http://api.airvisual.com/v2/nearest_city?lat=' + lat.toFixed(2) + '&lon=' + lon.toFixed(2) + '&key=8da422fd-653c-45f1-bce2-ca27fc2c8b30'
-  console.log(AQIUrl)
   fetch(AQIUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-
       var numberRaw = data.data.current.pollution.aqius
       var div1 = $('<div>').addClass('col s4 m6 custom-cards')
       var div2 = $('<div>').addClass('col card blue-grey darken-1 round-card')
@@ -83,30 +75,21 @@ function getRecData(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
       if (data.RECDATA.length === 0) {
         alert("No Data Available")
       } else {
         for (i = 0; i < data.RECDATA.length; i++) {
-          console.log(data.RECDATA.length)
           var div1 = $('<div>').addClass('col s4 custom-cards')
           console.log(div1)
           var div2 = $('<div>').addClass('col card round-card card horizontal')
           var div3 = $('<div>').addClass('card-content white-text')
           var title = $('<span>').addClass('card-title').text(data.RECDATA[i].RecAreaName)
-          console.log(title)
           var description = $('<p>').text(data.RECDATA[i].RecAreaDescription)
           var address = $('<p>').text(data.RECDATA[i].RECAREAADDRESS[0].RecAreaStreetAddress1 + " " + data.RECDATA[i].RECAREAADDRESS[0].PostalCode + " " + data.RECDATA[i].RECAREAADDRESS[0].City + " " + data.RECDATA[i].RECAREAADDRESS[0].AddressStateCode)
           var link = $('<a>')
           link.attr('href', data.RECDATA[i].LINK[0].URL).addClass('link')
           link.text(data.RECDATA[i].LINK[0].URL)
-          console.log(data.RECDATA[i].LINK[0].URL)
-          console.log(link)
-
-          console.log(data.RECDATA[i].RecAreaName)
-          console.log(data.RECDATA[i].RecAreaDescription)
-          console.log(data.RECDATA[i].RECAREAADDRESS[0].RecAreaStreetAddress1 + " " + data.RECDATA[i].RECAREAADDRESS[0].PostalCode + " " + data.RECDATA[i].RECAREAADDRESS[0].City + " " + data.RECDATA[i].RECAREAADDRESS[0].AddressStateCode)
-
+         
           div1.append(div2)
           div2.append(div3)
           div3.append(title, description, address, link)
@@ -126,7 +109,6 @@ function getRecData(lat, lon) {
 
 function backButton(e) {
   e.preventDefault();
-  console.log('working')
   startBtn.classList.remove('hide');
   inputText.classList.remove('hide');
   headOne.classList.remove('hide');
